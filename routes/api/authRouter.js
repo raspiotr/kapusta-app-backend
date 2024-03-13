4550726fbbb55a1fa23c1bdb512efb4bb28bbf9e
Mariusz-72 <mmbwdap@gmail.com>
2024-03-13 22:49:04 +0100

    update 2.0  - rejestracja i obsługa logowania  - podstawowa
    zmiany dotyczą przede wszystkim rejestracji użytkowników oraz dodania obsługi logowania użytkowników w aplikacji. Dodany router API dla rejestracji użytkowników (authRouter.js), kontrolery obsługujące rejestrację i logowanie użytkowników (authControllers.js), oraz dostosowałem plik app.js żeby zamontować nowy routera API do rejestracji użytkownika. Nie sprawdzałem działania  - potrzebuję do tego pomocy.

---
+++ routes/api/authRouter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { check } = require("express-validator");
+const { registerUser } = require("../../controllers/authControllers");
+
+const router = express.Router();
+
+
+router.post(
+    "/register",
+
+    [
+        check("name", "Name is required").notEmpty(),
+        check("email", "Please include a valid email").isEmail(),
+        check(
+        "password",
+        "Please enter a password with 5 or more characters"
+        ).isLength({ min: 5 }),
+    ],
+    registerUser
+);
+
+module.exports = router;
