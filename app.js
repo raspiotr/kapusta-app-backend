6ba9aab9a213ed52e5f7754f3301743b2716257d
raspiotr <raspiotr@gmail.com>
2024-03-14 10:29:13 +0100

    add / get categories

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const logger = require("morgan");
 
 const transactionsRouter = require("./routes/api/transactions");
+const categoriesRouter = require("./routes/api/transactionCategories");
 
 require("dotenv").config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/api/transactions", transactionsRouter);
+app.use("/api/categories", categoriesRouter);
 
 app.use((_, res, __) => {
   res.status(404).json({
