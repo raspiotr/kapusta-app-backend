4e8c5d6612ee176ea0a8a92cbbd1babea8fb8966
BGal23 <bartek.gal23@gmail.com>
2024-03-14 13:16:42 +0100

    heroku

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,49 +1,48 @@
 const bcrypt = require("bcrypt");
 const { validationResult } = require("express-validator");
-const { User } = require("../models/users");
+const { User } = require("../models/user");
 
 const registerUser = async (req, res) => {
   // Sprawdzenie błędów walidacji
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, password } = req.body;
+
+  try {
+    let existingUser = await User.findOne({ email }); // Sprawdzenie, czy użytkownik o podanym adresie e-mail już istnieje w bazie danych
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Użytkownik o podanym adresie e-mail już istnieje" });
     }
 
-    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10); // Haszowanie hasła użytkownika,
 
-    try {
-        let existingUser = await User.findOne({ email }); // Sprawdzenie, czy użytkownik o podanym adresie e-mail już istnieje w bazie danych
-        if (existingUser) {
-        return res
-            .status(400)
-            .json({ message: "Użytkownik o podanym adresie e-mail już istnieje" });
-        }
+    // Zapisanie nowego użytkownika do bazy danych
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
-        const hashedPassword = await bcrypt.hash(password, 10); // Haszowanie hasła użytkownika, 
+    await newUser.save();
 
-        // Zapisanie nowego użytkownika do bazy danych
-        const newUser = new User({
-        name,
-        email,
-        password: hashedPassword,
-        });
+    // Tutaj można  wysłać e-mail z linkiem potwierdzającym - do uzupełnienia potem
 
-        await newUser.save();
+    res
+      .status(201)
+      .json({ message: "Użytkownik został pomyślnie zarejestrowany" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Wystąpił błąd podczas rejestracji użytkownika" });
+  }
+};
 
-        // Tutaj można  wysłać e-mail z linkiem potwierdzającym - do uzupełnienia potem
-        
-
-        res
-        .status(201)
-        .json({ message: "Użytkownik został pomyślnie zarejestrowany" });
-    } catch (error) {
-        console.error(error);
-        res
-        .status(500)
-        .json({ message: "Wystąpił błąd podczas rejestracji użytkownika" });
-    }
-    };
-
-    module.exports = {
-    registerUser,
+module.exports = {
+  registerUser,
 };
--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const { check } = require("express-validator");
-const { registerUser } = require("../../controllers/authControllers");
+const { registerUser } = require("../../controllers/authController");
 
 const router = express.Router();
 
-
 router.post(
-    "/register",
+  "/register",
 
-    [
-        check("name", "Name is required").notEmpty(),
-        check("email", "Please include a valid email").isEmail(),
-        check(
-        "password",
-        "Please enter a password with 5 or more characters"
-        ).isLength({ min: 5 }),
-    ],
-    registerUser
+  [
+    check("name", "Name is required").notEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 5 or more characters"
+    ).isLength({ min: 5 }),
+  ],
+  registerUser
 );
 
 module.exports = router;
