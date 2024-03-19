530f8285d84fe305a8d8540dbc469b5912737954
Mariusz-72 <mmbwdap@gmail.com>
2024-03-19 20:31:45 +0100

    update 4.0

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const cors = require("cors");
 const express = require("express");
 const logger = require("morgan");
-const passport = require("passport");
 const session = require("express-session");
-
-const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
-require("dotenv").config();
+const passport = require("./middlewares/passport-config");
 
 const authRouter = require("./routes/api/authRouter");
 const transactionsRouter = require("./routes/api/transactions");
@@ -36,33 +33,35 @@ app.use(
   })
 );
 
+
+
+// Serializacja i deserializacja użytkownika
+//passport.serializeUser((user, done) => {
+//  done(null, user);
+//});
+
+//passport.deserializeUser((obj, done) => {
+//  done(null, obj);
+//});
+
+// konfiguracja logowania przez Google
+//passport.use(
+//  new GoogleStrategy(
+//    {
+//      clientID: process.env.GOOGLE_CLIENT_ID,
+//      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+//    },
+//    (accessToken, refreshToken, profile, done) => {
+//      // dane profilu użytkownika:
+//      return done(null, profile);
+//    }
+//  )
+//);
+
 // Inicjalizacja sesji Passport
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Serializacja i deserializacja użytkownika
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
-
-// konfiguracja logowania przez Google
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      // dane profilu użytkownika:
-      return done(null, profile);
-    }
-  )
-);
 
 // routing dla logowania przez google
 app.get(
@@ -78,19 +77,19 @@ app.get(
     res.redirect("/");
   }
 );
-
+      //obsł. ścieżek dla api:
 app.use("/api/transactions", authenticateToken, transactionsRouter);
 app.use("/api/categories", categoriesRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/reports", authenticateToken, reportsRouter);
 app.use("/api/user", authenticateToken, userRouter);
-
+      //obsł. błędnych ścieżek
 app.use((_, res, __) => {
   res.status(404).json({
     message: "Not found",
   });
 });
-
+      //obsł.błedów
 app.use((err, _, res, __) => {
   res.status(500).json({
     message: err.message,
