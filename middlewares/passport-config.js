bfd9e0d4d06676324fb0d10c70ef0b36ff9db1f6
raspiotr <raspiotr@gmail.com>
2024-03-20 14:06:20 +0100

    Update passport-config.js

--- middlewares/passport-config.js
+++ middlewares/passport-config.js
@@ -15,7 +15,8 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/api/auth/google/callback",
+        callbackURL:
+          "https://kapusta-backend-827563b0830f.herokuapp.com/api/auth/google/callback",
       },
       async (_, __, profile, done) => {
         try {
