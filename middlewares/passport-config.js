8e279744d54d92967f42b17c1861d23cc20bd494
raspiotr <raspiotr@gmail.com>
2024-03-28 16:04:42 +0100

    update server address

--- middlewares/passport-config.js
+++ middlewares/passport-config.js
@@ -16,7 +16,7 @@ module.exports = function (passport) {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL:
-          "https://kapusta-backend-827563b0830f.herokuapp.com/api/auth/google/callback",
+          "https://kapusta-app-eafad5d610ef.herokuapp.com/api/auth/google/callback",
       },
       async (_, __, profile, done) => {
         try {
